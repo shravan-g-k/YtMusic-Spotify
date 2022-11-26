e44c3c6c97ece8a01c4b80cfb9027cf1c46655c0
Shravan G K <shravangkulal.programs@gmail.com>
2022-11-26 23:52:53 +0530

    convert json to py and print song names

--- yt_to_spotify/add_to_spotify.py
+++ yt_to_spotify/add_to_spotify.py
@@ -1,12 +1,26 @@
 import spotipy
 from spotipy.oauth2 import SpotifyClientCredentials
-from json import dumps
+from json import dumps,loads
+
+
+with open("sp_song.json","w") as file:
+    file.write("")
+
+
 sp = spotipy.Spotify(auth_manager=SpotifyClientCredentials(
     client_id="17a02e14ae594f5a97d77cabe7c2ee3a", client_secret="49dd7ce7271c4b9a9c833a00bbcef22c"))
 
 
-response = sp.search(q="marethuhoyithe",market="IN",type="track")
+response  = sp.search(q="ninadene Januma",market="IN",type="track")
 
 response_js = dumps(response)
+response_dict = response_js
 with open("sp_song.json","w") as file:
     file.write(str(response_js))
+
+with open("sp_song.json","r") as file:
+    read_js = file.read()
+    read = loads(read_js)
+
+for indx,i in enumerate(read["tracks"]["items"]):
+    print("*",indx+1,i["name"])
